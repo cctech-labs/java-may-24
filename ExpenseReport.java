f9fa794222f74d69c897a4207c156afc9fcd40af
SarawadeShrutika <shrutikasa598@gmail.com>
2025-05-24 13:38:02 +0530

    Updated the limit condition

--- ExpenseReport.java
+++ ExpenseReport.java
@@ -40,7 +40,7 @@ public class ExpenseReport {
                 break;
             }
 
-            String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 || expense.type == ExpenseType.LUNCH && expense.amount > 2000   ? "X" : " ";
+            String mealOverExpensesMarker = expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 || expense.type == ExpenseType.LUNCH && expense.amount <= 2000   ? "X" : " ";
 
             System.out.println(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker);
 
